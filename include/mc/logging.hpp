df4515ed95a75d310a45c1cd989d163f54500893
Al-Andrew <aldea.andrei.977@gmail.com>
2023-06-03 19:50:33 +0300

    [logging] introduce spdlog logging

--- include/mc/logging.hpp
+++ include/mc/logging.hpp
@@ -1,4 +1,8 @@
 #pragma once
+#ifndef SPDLOG_ACTIVE_LEVEL
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+#endif
+#include "spdlog/sinks/stdout_sinks.h"
 #include "spdlog/spdlog.h"
 
 #include <optional>
@@ -9,5 +13,22 @@ namespace logging {
 std::optional<spdlog::level::level_enum>
 string_to_log_level(const std::string_view level);
 
+void
+init_logging(const spdlog::level::level_enum level);
+
+namespace detail {
+struct LoggersContainer {
+    static std::shared_ptr<spdlog::logger> stdout;
+};
+
+}  // namespace detail
+
 }  // namespace logging
 }  // namespace mc
+
+#define MC_TRACE(...)    SPDLOG_LOGGER_TRACE(mc::logging::detail::LoggersContainer::stdout, __VA_ARGS__)
+#define MC_DEBUG(...)    SPDLOG_LOGGER_DEBUG(mc::logging::detail::LoggersContainer::stdout, __VA_ARGS__)
+#define MC_INFO(...)     SPDLOG_LOGGER_INFO(mc::logging::detail::LoggersContainer::stdout, __VA_ARGS__)
+#define MC_WARN(...)     SPDLOG_LOGGER_WARN(mc::logging::detail::LoggersContainer::stdout, __VA_ARGS__)
+#define MC_ERROR(...)    SPDLOG_LOGGER_ERROR(mc::logging::detail::LoggersContainer::stdout, __VA_ARGS__)
+#define MC_CRITICAL(...) SPDLOG_LOGGER_CRITICAL(mc::logging::detail::LoggersContainer::stdout, __VA_ARGS__)
--- src/logging.cpp
+++ src/logging.cpp
@@ -1,6 +1,11 @@
 #include "mc/logging.hpp"
 
-#include "spdlog/common.h"
+#include "spdlog/logger.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/stdout_sinks.h"
+
+#include <memory>
+#include <optional>
 
 namespace mc::logging {
 using namespace std::string_view_literals;
@@ -19,6 +24,19 @@ string_to_log_level(const std::string_view level) {
     }
 
     return std::nullopt;
+}
+
+auto detail::LoggersContainer::stdout = spdlog::stdout_color_mt("console");
+void
+init_logging(const spdlog::level::level_enum level) {
+    using namespace detail;
+
+    LoggersContainer::stdout->set_level(level);
+    if (static_cast<unsigned int>(level) <= static_cast<unsigned int>(spdlog::level::level_enum::debug)) {
+        LoggersContainer::stdout->set_pattern("%^ [%R] [%-9l] [%@] %v %$");
+    } else {
+        LoggersContainer::stdout->set_pattern("%^ [%R] [%-9l] %v ^$");
+    }
 }
 
 }  // namespace mc::logging
