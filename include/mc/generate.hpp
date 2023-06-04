0d2346a9e960b9653e02ddbd644cb12e50d1640d
Al-Andrew <aldea.andrei.977@gmail.com>
2023-06-04 14:07:50 +0300

    [gen] basic templating

---
+++ include/mc/generate.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "mc/language_description.hpp"
+
+#include <fstream>
+
+namespace mc {
+
+void
+generate_makefile(Language_Description ld, std::ofstream &proj, std::ofstream &src) noexcept;
+
+void
+generate_lex_lexer(Language_Description ld, std::ofstream file) noexcept;
+
+void
+generate_yacc_parser(Language_Description ld, std::ofstream file) noexcept;
+
+void
+generate_ast(Language_Description ld, std::ofstream header, std::ofstream file) noexcept;
+
+void
+generate_executable(Language_Description ld) noexcept;
+
+void
+generate(Language_Description ld, std::filesystem::path output_dir) noexcept;
+
+}  // namespace mc
